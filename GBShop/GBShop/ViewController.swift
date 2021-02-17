5ff2ed853cdd2e55b789f830903838481535cbc8
Pavel Chernyshev <titan100695@yandex.ru>
2021-02-17 22:40:30 +0300

    initial

---
+++ GBShop/GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Павел Чернышев on 17.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
